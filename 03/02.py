a07be49e7992009c3e72c0399fbba5fa0a4cbb86
Jaka <vesoljec.jk@gmail.com>
2021-12-03 07:50:20 +0100

    03122021_1

--- 03/02.py
+++ 03/02.py
@@ -7,14 +7,13 @@ def most_common_bit(list, pos):
         else:
             zeros += 1
     return 1 if ones >= zeros else 0
-    return 0 if ones >= zeros else 1
+    return 0 if ones >= zeros else 1  # comment first line to make least common bit
 
 
 with open("03/input.txt", "r") as f:
     f = f.read().split("\n")
     g = f
     i = 0
-    # while(len(g) > 1):
     while(len(g) > 1):
         mcb = str(most_common_bit(g, i))
         torem = []
