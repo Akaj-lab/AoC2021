bc6fd8897b9994e27381f0d5a70634e7a2ff24d0
Jaka <vesoljec.jk@gmail.com>
2021-12-07 08:04:09 +0100

    07122021

---
+++ 07/01.py
@@ -0,0 +1,23 @@
+import numpy
+
+
+def factorial_sum(x):
+    o = 0
+    for i in range(x+1):
+        o += i
+    return o
+
+
+with open("input.txt", "r") as f:
+    f = f.read().split(",")
+    sum = 0
+    a = []
+    for i in f:
+        a.append(int(i))
+    # zaradi specifičnega inpute, more zavkrožit navzgor
+    avg = numpy.median(a)
+    print(avg)
+    fuel = 0
+    for i in a:
+        fuel += factorial_sum(abs(avg-i))
+    print(fuel)
